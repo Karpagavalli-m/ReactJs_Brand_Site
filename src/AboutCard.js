3716a4fb855828d0562e5a157396b75b66f3b7f6
Karpagavalli-m <karpagavallim0401@gmail.com>
2024-11-26 21:21:32 +0530

    fourth commit

--- src/AboutCard.js
+++ src/AboutCard.js
@@ -16,12 +16,12 @@ const AboutCard = () => {
         { title: 'Registered Office Address' ,
             subtitle: ['Flipkart Internet Private Limited,  Buildings Alyssa Begonia & Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village,-Bengaluru, 560103, Karnataka, India','CIN: U51109KA2012PTC066107','Telephone: 044-45614700/044-67415800']}
     ]
-
+    
   return (
    <Container fluid className='bg-dark g-5'>
     <Row className='bg-dark justify-content-around' >
         {aboutItems.map((item,idx)=>(
-            <Col key={idx} className='g-3' md={2} xs={4}>
+            <Col key={idx} className='g-3' md={2} xs={5}>
                 <Card className=' bg-dark  text-white  align-items-center ps-4'>
                     <div className='text-start'>
                     <Card.Text className='text-secondary fw-bold'>{item.title}</Card.Text>
@@ -32,13 +32,13 @@ const AboutCard = () => {
                 </Card>
             </Col>
         ))}
-        <Col className='g-3' md={2} xs={4}>
+        <Col className='g-3' md={2} xs={5}>
                 <Card className=' bg-dark  text-white  align-items-center ps-4'>
                     <div className='text-start'>
                     <Card.Text className='text-secondary fw-bold'>Mail Us</Card.Text>       
                         <Card.Text >Flipkart Internet Private Limited,  Buildings Alyssa Begonia & Clove Embassy Tech Village, Outer Ring Road, Devarabeesanahalli Village,-Bengaluru, 560103, Karnataka, India</Card.Text>
                     <Card.Text className='text-secondary fw-bold'>Social Media</Card.Text>
-                    <Card.Text className={`d-flex justify-content-between pe-5 me-5`} id="sociamediaIcon" xs={1}>
+                    <Card.Text className={`d-flex justify-content-between pe-5 me-5`} id="socialmediaIcon"  xs={1}>
                         <p><FaFacebook /></p>
                         <p><FaXTwitter /></p>
                         <p><FaYoutube /></p>
