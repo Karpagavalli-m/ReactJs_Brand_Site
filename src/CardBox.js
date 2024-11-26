303666553efd6b5c1fba441ac17f16eaad4ec854
Karpagavalli-m <karpagavallim0401@gmail.com>
2024-11-27 00:24:19 +0530

    fifth commit

--- src/CardBox.js
+++ src/CardBox.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import { IoIosArrowDroprightCircle } from 'react-icons/io'
 import Cards from './Cards'
 import ItemsHeader from './ItemsHeader'
 
--- src/VerticalScrollItems.js
+++ src/VerticalScrollItems.js
@@ -6,7 +6,7 @@ const VerticalScrollItems = ({array}) => {
     <Container className='d-flex d-lg-none overflow-x-auto bg-light' fluid>  
     <Stack direction='horizontal' className='d-lg-none mb-3'>
       {array.map((item, idx) => (
-        <Col key={idx} className='' xs={4} sm={4}>
+        <Col key={idx} className='p-2' xs={4} sm={5}>
           <Card className='align-items-center text-center' >
           <Card.Img variant="top" src={require(`${item.image}`)}
           id="card-image" />
